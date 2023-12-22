334d7e5bcc8e455918706b5bdcf83a69290bba4b
Bill Bindi <billdbindi@gmail.com>
2023-12-22 08:20:28 -0800

    Euler 11.

--- src/main/java/euler/ProjectEuler.java
+++ src/main/java/euler/ProjectEuler.java
@@ -10,27 +10,6 @@ import java.util.stream.LongStream;
 
 public final class ProjectEuler {
 
-    private static final String EULER_8_INPUT = "73167176531330624919225119674426574742355349194934" +
-            "96983520312774506326239578318016984801869478851843" +
-            "85861560789112949495459501737958331952853208805511" +
-            "12540698747158523863050715693290963295227443043557" +
-            "66896648950445244523161731856403098711121722383113" +
-            "62229893423380308135336276614282806444486645238749" +
-            "30358907296290491560440772390713810515859307960866" +
-            "70172427121883998797908792274921901699720888093776" +
-            "65727333001053367881220235421809751254540594752243" +
-            "52584907711670556013604839586446706324415722155397" +
-            "53697817977846174064955149290862569321978468622482" +
-            "83972241375657056057490261407972968652414535100474" +
-            "82166370484403199890008895243450658541227588666881" +
-            "16427171479924442928230863465674813919123162824586" +
-            "17866458359124566529476545682848912883142607690042" +
-            "24219022671055626321111109370544217506941658960408" +
-            "07198403850962455444362981230987879927244284909188" +
-            "84580156166097919133875499200524063689912560717606" +
-            "05886116467109405077541002256983155200055935729725" +
-            "71636269561882670428252483600823257530420752963450";
-
     public static void main(String[] args) {
 //        System.out.println(euler1(1000));
 //        System.out.println(euler2(4_000_000));
@@ -39,9 +18,10 @@ public final class ProjectEuler {
 //        System.out.println(euler5(20));
 //        System.out.println(euler6(100));
 //        System.out.println(euler7(10_001));
-//        System.out.println(euler8(EULER_8_INPUT, 13));
+//        System.out.println(euler8(Inputs.EULER_8_INPUT, 13));
 //        System.out.println(euler9(1000));
-        System.out.println(euler10(2_000_000));
+//        System.out.println(euler10(2_000_000));
+        System.out.println(euler11(Inputs.EULER_11_INPUT, 4));
     }
 
     /**
@@ -179,5 +159,67 @@ public final class ProjectEuler {
             prime = generator.next();
         }
         return sum;
+    }
+
+    /**
+     * What is the greatest product of {@code n} adjacent numbers in the same direction
+     * (up, down, left, right, or diagonally) in the given grid?
+     */
+    private static long euler11(int[][] grid, int n) {
+        long max = 0;
+
+        // vertical
+        for (int i = 0; i <= grid.length - n; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                long product = 1;
+                for (int k = 0; k < n; k++) {
+                    product *= grid[i + k][j];
+                }
+                if (product > max) {
+                    max = product;
+                }
+            }
+        }
+
+        // horizontal
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j <= grid[i].length - n; j++) {
+                long product = 1;
+                for (int k = 0; k < n; k++) {
+                    product *= grid[i][j + k];
+                }
+                if (product > max) {
+                    max = product;
+                }
+            }
+        }
+
+        // diagonal down-right
+        for (int i = 0; i <= grid.length - n; i++) {
+            for (int j = 0; j <= grid[i].length - n; j++) {
+                long product = 1;
+                for (int k = 0; k < n; k++) {
+                    product *= grid[i + k][j + k];
+                }
+                if (product > max) {
+                    max = product;
+                }
+            }
+        }
+
+        // diagonal up-right
+        for (int i = n - 1; i < grid.length; i++) {
+            for (int j = 0; j <= grid[i].length - n; j++) {
+                long product = 1;
+                for (int k = 0; k < n; k++) {
+                    product *= grid[i - k][j + k];
+                }
+                if (product > max) {
+                    max = product;
+                }
+            }
+        }
+
+        return max;
     }
 }
