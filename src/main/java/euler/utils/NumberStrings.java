9bb8141792ef61b43c45218df5da4e9ac90b9a59
Bill Bindi <billdbindi@gmail.com>
2023-12-22 07:45:53 -0800

    Euler 8.

--- src/main/java/euler/utils/NumberStrings.java
+++ src/main/java/euler/utils/NumberStrings.java
@@ -1,11 +1,27 @@
 package euler.utils;
 
+import com.google.common.base.Preconditions;
+
 public final class NumberStrings {
 
     public static boolean isPalindrome(long n) {
         String stringN = String.valueOf(n);
         String reverse = new StringBuilder(stringN).reverse().toString();
         return stringN.equals(reverse);
+    }
+
+    public static long product(String number, int startInclusive, int endExclusive) {
+        Preconditions.checkArgument(startInclusive >= 0 &&
+                startInclusive < endExclusive &&
+                endExclusive <= number.length(),
+                "Invalid indices [%d, %d).",
+                startInclusive,
+                endExclusive);
+        long product = 1;
+        for (int i = startInclusive; i < endExclusive; i++) {
+            product *= Character.getNumericValue(number.charAt(i));
+        }
+        return product;
     }
 
     private NumberStrings() {}
